7abfe01c4aa77ac6a8efb4c3c27d1d59f02902b6
Alyne Martins <martins.alyne@gmail.com>
2018-06-05 21:45:33 -0300

    JavaScript - invertendo array e convertendo para Inteiro

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var digits = prompt("Digite o numero do seu cartão");
+var newArray = digitos.split('');
+console.log(newArray);
+var array = convertingToInteger(newArray);
+
+// Transformando dígitos em números Inteiros
+function convertingToInteger(newArray){
+  for (var i in newArray ){
+newArray[i] = parseInt(newArray[i]);
+  console.log(newArray);
+  }
+ return newArray;
+}
+
+
+array = array.reverse();
+
+console.log(array);
+
